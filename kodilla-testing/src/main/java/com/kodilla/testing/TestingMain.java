55041116563d736196465a60792c0013cbdff3de
ozmmik <ozmin@onet.pl>
2018-12-17 14:02:31 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania opragromowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String results = simpleUser.getUsername();
+
+        if (results.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // *** Kolejny test ***
+
+        Calculator calculator = new Calculator();
+
+        int add = calculator.add(5, 4);
+        int sub = calculator.substract(10, 5);
+
+        System.out.println("Add: " + add + " Substract: " + sub);
+
+
     }
+
 }
