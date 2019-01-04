ac8d3b0f343a09c25681eb3d5e027deb4fd5f982
ozmmik <ozmin@onet.pl>
2019-01-04 20:07:11 +0100

    Module 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/lambda/ExpressionExecutor.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/ExpressionExecutor.java
@@ -1,6 +1,6 @@
 package com.kodilla.stream.lambda;
 
-public class ExpressionExcutor {
+public class ExpressionExecutor {
     public void executeExpression(double a, double b, MathExpression mathExpression){
     double result = mathExpression.calculateExpression(a, b);
         System.out.println("Result equals:" + result);
